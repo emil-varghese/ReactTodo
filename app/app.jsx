dd7527f22daad0c9dcea877da56d627ed77d106e
Emil Varghese <emilvpulinattu@gmail.com>
2016-12-21 16:40:30 -0800

    Setup

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
 // Load foundation
 $(document).foundation();
 
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function() {
+    return (
+      <div>
+        todo.jsx
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
